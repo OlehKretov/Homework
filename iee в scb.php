827db5a1b700015a239c4ca41bc526b378a4c8dc
OlehKretov <olkret80@gmail.com>
2016-12-15 14:39:35 +0200

    Fix iee to scb

--- iee в scb.php
+++ iee в scb.php
@@ -4,12 +4,18 @@ echo "Give it to me!\n";
 $handle = fopen ("php://stdin","r");
 $number = fgets($handle);
 
-if ($number > 100) {
-    echo "Thanks, man!";
-} elseif (($number > 10) && ($number < 100)) {
-    echo "OK :(";
-} else echo "WHAAAAT????";
-
-echo "\n";
-if ($number > 1000) echo "\n!!!!WOOOOWWWW!!!\n";
+switch ($number) {
+	case ($number > 1000):
+		echo "\n!!!!WOOOOWWWW!!!\n";
+		exit;
+	case ($number > 100):
+		echo "Thanks, man!";
+		break;
+	case (($number > 10) && ($number < 100)):
+		echo "OK :(";
+		break;
+	default:
+		echo "WHAAAAT????";
+		break;
+}
 ?>
